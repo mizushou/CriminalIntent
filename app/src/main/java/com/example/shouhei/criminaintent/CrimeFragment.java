365b6380cf4f1ae6ae58d35502893bac0a1815d4
Shouhei.Mizuno <32non23@gmail.com>
2018-05-31 18:55:20 -0700

    implemented chapter10

--- app/src/main/java/com/example/shouhei/criminaintent/CrimeFragment.java
+++ app/src/main/java/com/example/shouhei/criminaintent/CrimeFragment.java
@@ -14,17 +14,33 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 
+import java.util.UUID;
+
 public class CrimeFragment extends Fragment {
 
+  private static final String ARG_CRIME_ID = "crime_id";
   private Crime mCrime;
   private EditText mTitleField;
   private Button mDateButton;
   private CheckBox mSolvedCheckBox;
 
+  public static CrimeFragment newInstance(UUID crimeId) {
+    Bundle args = new Bundle();
+    args.putSerializable(ARG_CRIME_ID, crimeId);
+
+    CrimeFragment fragment = new CrimeFragment();
+    fragment.setArguments(args);
+    return fragment;
+  }
+
   @Override
   public void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    mCrime = new Crime();
+    //    mCrime = new Crime();
+    //    UUID crimeId =
+    //        (UUID) getActivity().getIntent().getSerializableExtra(CrimeActivity.EXTRA_CRIME_ID);
+    UUID crimeId = (UUID) getArguments().getSerializable(ARG_CRIME_ID);
+    mCrime = CrimeLab.get(getActivity()).getCrime(crimeId);
   }
 
   @Nullable
@@ -36,6 +52,7 @@ public class CrimeFragment extends Fragment {
     View v = inflater.inflate(R.layout.fragment_crime, container, false);
 
     mTitleField = v.findViewById(R.id.crime_title);
+    mTitleField.setText(mCrime.getTitle());
     mTitleField.addTextChangedListener(
         new TextWatcher() {
           @Override
@@ -55,6 +72,7 @@ public class CrimeFragment extends Fragment {
     mDateButton.setEnabled(false);
 
     mSolvedCheckBox = v.findViewById(R.id.crime_solved);
+    mSolvedCheckBox.setChecked(mCrime.isSolved());
     mSolvedCheckBox.setOnCheckedChangeListener(
         new CompoundButton.OnCheckedChangeListener() {
           @Override
--- app/src/main/java/com/example/shouhei/criminaintent/CrimeListFragment.java
+++ app/src/main/java/com/example/shouhei/criminaintent/CrimeListFragment.java
@@ -1,5 +1,6 @@
 package com.example.shouhei.criminaintent;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -35,12 +36,23 @@ public class CrimeListFragment extends Fragment {
     return view;
   }
 
+  @Override
+  public void onResume() {
+    super.onResume();
+    updateUI();
+  }
+
   private void updateUI() {
     CrimeLab crimeLab = CrimeLab.get(getActivity());
     List<Crime> crimes = crimeLab.getCrimes();
 
-    mAdapter = new CrimeAdapter(crimes);
-    mCrimeRecyclerView.setAdapter(mAdapter);
+    if (mAdapter == null) {
+      mAdapter = new CrimeAdapter(crimes);
+      mCrimeRecyclerView.setAdapter(mAdapter);
+    } else {
+      mAdapter.notifyDataSetChanged();
+
+    }
   }
 
   private class CrimeHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
@@ -68,7 +80,11 @@ public class CrimeListFragment extends Fragment {
 
     @Override
     public void onClick(View v) {
-      Toast.makeText(getActivity(), mCrime.getTitle() + " clicked!", Toast.LENGTH_SHORT).show();
+      //      Toast.makeText(getActivity(), mCrime.getTitle() + " clicked!",
+      // Toast.LENGTH_SHORT).show();
+      //      Intent intent = new Intent(getActivity(), CrimeActivity.class);
+      Intent intent = CrimeActivity.newIntent(getActivity(), mCrime.getId());
+      startActivity(intent);
     }
   }
 
